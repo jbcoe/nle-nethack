d9f3e48a03827240edf938e97edb954c98114ebb
Jonathan B. Coe <jonathanbcoe@gmail.com>
2026-02-01 16:17:57 +0000

    Use ruff rather than black and isort for linting (#75)
    * Use ruff rather than black and isort for linting

--- nle/nethack/__init__.py
+++ nle/nethack/__init__.py
@@ -1,16 +1,14 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
-from nle.nethack.actions import *  # noqa: F403
 from nle._pynethack.nethack import *  # noqa: F403
-from nle.nethack.nethack import (
-    Nethack,
-    NETHACKOPTIONS,
-    DUNGEON_SHAPE,
-    BLSTATS_SHAPE,
-    MESSAGE_SHAPE,
-    INV_SIZE,
-    PROGRAM_STATE_SHAPE,
-    INTERNAL_SHAPE,
-    OBSERVATION_DESC,
-    TTYREC_VERSION,
-    tty_render,
-)
+from nle.nethack.actions import *  # noqa: F403
+from nle.nethack.nethack import BLSTATS_SHAPE
+from nle.nethack.nethack import DUNGEON_SHAPE
+from nle.nethack.nethack import INTERNAL_SHAPE
+from nle.nethack.nethack import INV_SIZE
+from nle.nethack.nethack import MESSAGE_SHAPE
+from nle.nethack.nethack import NETHACKOPTIONS
+from nle.nethack.nethack import OBSERVATION_DESC
+from nle.nethack.nethack import PROGRAM_STATE_SHAPE
+from nle.nethack.nethack import TTYREC_VERSION
+from nle.nethack.nethack import Nethack
+from nle.nethack.nethack import tty_render
